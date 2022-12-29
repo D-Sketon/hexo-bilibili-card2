676a8f4cbe82e212a0d551bbda7b253f705c9ded
D-Sketon <2055272094@qq.com>
2022-12-29 22:48:44 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# hexo-bilibili-card2
+基于 [hexo-bilibili-card](https://github.com/MaxChang3/hexo-bilibili-card) 的魔改版
+
+
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+const fs = require('hexo-fs');
+const path = require('path');
+const css = hexo.extend.helper.get('css').bind(hexo);
+const bili = require('./lib/bili.js')
+
+hexo.extend.generator.register('bili_asset', () => [{
+  path: 'css/bilicard.css',
+  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "bilicard.css"))
+}
+]);
+
+hexo.extend.generator.register('bili_play', () => [{
+  path: 'svg/play.svg',
+  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "play.svg"))
+}
+]);
+
+hexo.extend.generator.register('bili_danmaku', () => [{
+  path: 'svg/danmaku.svg',
+  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "danmaku.svg"))
+}
+]);
+
+hexo.extend.generator.register('bili_up', () => [{
+  path: 'svg/up.svg',
+  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "up.svg"))
+}
+]);
+
+hexo.extend.tag.register("bilicard2", async (args) => {
+  return await bili(args[0]);
+}, { async: true });
+
+hexo.extend.injector.register('head_begin', () => {
+  return css('/css/bilicard.css');
+}, 'post')
